f99ebc6a50b5bd4bfe0c8fd8d25fc78704d4c163
Afshan Tayyab <tayyaba044705@gmail.com>
2026-03-15 22:17:40 +0530

    Refactor word input and search button logic
    Removed unused input and search button logic for Pexels API.

--- twoapi.py
+++ twoapi.py
@@ -2,32 +2,16 @@ import streamlit as st
 import requests
 
 st.title("Word Explorer App")
-
-# Input box
 word = st.text_input("Enter a word")
-
-# Search button
 search = st.button("Search")
-
-# Run only when button is clicked
 if search and word:
-
-    # Datamuse API (similar words)
     datamuse_url = f"https://api.datamuse.com/words?ml={word}"
     response = requests.get(datamuse_url)
     words = response.json()
-
     st.subheader("Similar Words")
-
     for w in words[:5]:
         st.write(w["word"])
-
-
-    # Pexels API Key
     PEXELS_API_KEY = "DcogTvUQtXP9acjIPBQdWWSJXw46wQ9WrL5soRTc4Xh7SptwowpjeM52"   
-
-
-    # Pexels API request
     pexels_url = f"https://api.pexels.com/v1/search?query={word}&per_page=5"
 
     headers = {
